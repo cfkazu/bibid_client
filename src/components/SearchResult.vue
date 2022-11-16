5572e12f48fc284a5ed72dc12ea0063d6ad7f12f
CFkazu <kkkazu@g.ecc.u-tokyo.ac.jp>
2022-11-17 01:52:52 +0900

    検索結果に作者欄

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -11,13 +11,43 @@
                         <v-list-item-content>
                             <v-row class="mb-6" no-gutters>
 
-                                <v-col v-for="list in lists" :key="list.id">
-                                    <router-link :to="'/image/' + list.id">
-                                        <v-img :max-width="$vuetify.breakpoint.width / (dividenum + 1)"
-                                            :aspect-ratio="1" v-bind:src="list.image">
-                                        </v-img>
-                                    </router-link>
+                                <v-col v-for="list in lists" :key="list.id" md="2.4">
+                                    <v-col md="12">
+                                        <v-card loading="false" class="mx-auto my-12" max-width="374">
+                                            <router-link :to="'/image/' + list.id">
+                                                <v-img :max-width="$vuetify.breakpoint.width / (dividenum)"
+                                                    :aspect-ratio="1" v-bind:src="list.image">
+                                                </v-img>
+                                            </router-link>
+                                            <br>
+                                            <div class="search-about__contents-text" align="left">{{ list.title }}</div>
+                                            <v-card-actions>
 
+                                                <v-list-item class="grow">
+
+
+                                                    <v-list-item-avatar color="grey darken-3">
+                                                        <v-img class="elevation-6" alt=""
+                                                            :src="list.author_id.last_name">
+                                                        </v-img>
+                                                    </v-list-item-avatar>
+
+                                                    <v-list-item-content>
+                                                        <v-list-item-title>{{ list.author_id.username }}
+                                                        </v-list-item-title>
+                                                    </v-list-item-content>
+
+                                                    <v-row align="center" justify="end">
+                                                        <v-icon class="mr-1">
+                                                            mdi-heart
+                                                        </v-icon>
+                                                        <span class="subheading mr-2">{{ list.good }}</span>
+                                                    </v-row>
+                                                </v-list-item>
+                                            </v-card-actions>
+                                        </v-card>
+
+                                    </v-col>
                                 </v-col>
                             </v-row>
 
@@ -119,4 +149,18 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import '../styles/common/common';
+
+.search-result__contents {
+    margin-top: 20px;
+    justify-content: center;
+
+    &-img {
+        text-align: center;
+    }
+
+    &-text {
+        margin-left: 20px;
+        word-wrap: break-word;
+    }
+}
 </style>
